35bbda68b8e3f5b335a2826d5ec969f8887858b9
Mick Darling <184286+mickdarling@users.noreply.github.com>
2025-09-10 15:05:16 -0400

    chore: Remove unrelated test files

--- test_unicode.js
+++
@@ -1,7 +0,0 @@
-const { UnicodeValidator } = require('./dist/security/validators/unicodeValidator.js');
-
-const rtlName = '\u202Eeruces';
-const result = UnicodeValidator.normalize(rtlName);
-console.log('Input:', rtlName);
-console.log('Normalized:', result.normalizedContent);
-console.log('Is "secure"?:', result.normalizedContent === 'secure');
--- test_unicode.mjs
+++
@@ -1,7 +0,0 @@
-import { UnicodeValidator } from './dist/security/validators/unicodeValidator.js';
-
-const rtlName = '\u202Eeruces';
-const result = UnicodeValidator.normalize(rtlName);
-console.log('Input:', rtlName);
-console.log('Normalized:', result.normalizedContent);
-console.log('Is "secure"?:', result.normalizedContent === 'secure');
